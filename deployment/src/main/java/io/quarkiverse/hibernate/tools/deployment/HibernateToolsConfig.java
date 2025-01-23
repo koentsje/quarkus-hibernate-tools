3c71e33eaeae35c60c76c7ed54cb252c730e460e
Koen Aers <koen.aers@gmail.com>
2025-01-23 15:08:25 +0200

    Add class 'HibernateToolsConfig' and use this configuration during deployment
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ deployment/src/main/java/io/quarkiverse/hibernate/tools/deployment/HibernateToolsConfig.java
@@ -0,0 +1,27 @@
+package io.quarkiverse.hibernate.tools.deployment;
+
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
+
+@ConfigMapping(prefix = "quarkus.datasource")
+@ConfigRoot(phase = ConfigPhase.BUILD_TIME)
+public interface HibernateToolsConfig {
+
+    /**
+     * the datasource jdbc properties
+     */
+    Jdbc jdbc();
+
+    interface Jdbc {
+
+        /**
+         * the datasource jdbc url
+         */
+        @WithDefault("<datasource jdbc url>")
+        String url();
+
+    }
+
+}
--- deployment/src/main/java/io/quarkiverse/hibernate/tools/deployment/HibernateToolsProcessor.java
+++ deployment/src/main/java/io/quarkiverse/hibernate/tools/deployment/HibernateToolsProcessor.java
@@ -2,13 +2,18 @@ package io.quarkiverse.hibernate.tools.deployment;
 
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.logging.Log;
 
 class HibernateToolsProcessor {
 
     private static final String FEATURE = "hibernate-tools";
 
+    private HibernateToolsConfig hibernateToolsConfig;
+
     @BuildStep
     FeatureBuildItem feature() {
+        Log.info("Building FeatureBuildItem: " + FEATURE);
+        Log.info("  - Configured datasource : " + hibernateToolsConfig.jdbc().url());
         return new FeatureBuildItem(FEATURE);
     }
 }
