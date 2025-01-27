7cb1de566e464dbbb5432c6425b73881f7420cb1
Koen Aers <koen.aers@gmail.com>
2025-01-27 14:32:00 +0100

    Contribute a console command to the dev console
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- deployment/src/main/java/io/quarkiverse/hibernate/tools/deployment/HibernateToolsProcessor.java
+++ deployment/src/main/java/io/quarkiverse/hibernate/tools/deployment/HibernateToolsProcessor.java
@@ -1,7 +1,14 @@
 package io.quarkiverse.hibernate.tools.deployment;
 
+import org.aesh.command.Command;
+import org.aesh.command.CommandDefinition;
+import org.aesh.command.CommandException;
+import org.aesh.command.CommandResult;
+import org.aesh.command.invocation.CommandInvocation;
+
 import io.quarkiverse.hibernate.tools.runtime.HibernateToolsConfig;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.ConsoleCommandBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.logging.Log;
 
@@ -17,4 +24,20 @@ class HibernateToolsProcessor {
         Log.info("  - Configured datasource : " + hibernateToolsConfig.jdbc().url());
         return new FeatureBuildItem(FEATURE);
     }
+
+    @BuildStep
+    ConsoleCommandBuildItem revengCommand() {
+        return new ConsoleCommandBuildItem(new RevengCommand());
+    }
+
+    @CommandDefinition(name = "reveng", description = "Perform reverse engineering from the database", aliases = { "r" })
+    public static class RevengCommand implements Command {
+
+        @Override
+        public CommandResult execute(CommandInvocation commandInvocation) throws CommandException, InterruptedException {
+            commandInvocation.getShell().writeln("Hello from Reveng execution in Quarkus Terminal!");
+            return CommandResult.SUCCESS;
+        }
+    }
+
 }
